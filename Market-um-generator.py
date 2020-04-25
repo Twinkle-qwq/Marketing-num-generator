82773f4da0b307c4f1866ae6c51430de87c264ed
HANNARI <53106606+Twinkle-qwq@users.noreply.github.com>
2020-04-25 18:30:33 +0800

    Add files via upload

---
+++ Market-um-generator.py
@@ -0,0 +1,8 @@
+# coding = utf-8
+
+sub = str(input("请输入主体:"))
+sth = str(input("请输入事件:"))
+anoword = str(input("请输入另一种说法:"))
+print(f"{sub}{sth}是怎么回事呢？{sub}相信大家都很熟悉，但是{sub}{sth}是怎么回事呢，下面就让小编带大家一起了解吧。")
+print(f"{sub}{sth}，其实就是{anoword}，大家可能会很惊讶{sub}怎么会{sth}呢？但事实就是这样，小编也感到非常惊讶。")
+print(f"这就是关于{sub}{sth}的事情了，大家有什么想法呢，欢迎在评论区告诉小编一起讨论哦！")
